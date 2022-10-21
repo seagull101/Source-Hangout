f46852a9ed2a0bb0d2e4dba5d10bf4ccb590506d
Codecrafter <41242242+seagull101@users.noreply.github.com>
2022-10-21 15:40:37 -0700

    Added Resources
    Handles UI and subtitles.

---
+++ resource/CaptionCompiler.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Steam\steamapps\common\Source SDK Base 2013 Multiplayer\bin\captioncompiler.exe" %1 -game "C:\Program Files (x86)\Steam\steamapps\sourcemods\MalMetro\"
+pause
