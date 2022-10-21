1af276c84b74f619f615c0bd7cd4ac432a401dae
Codecrafter <41242242+seagull101@users.noreply.github.com>
2022-10-21 15:08:22 -0700

    Added Misc Files
    IDK what these do, but I think they are important.

---
+++ start.bat
@@ -0,0 +1,9 @@
+@echo off
+cls
+title srcds.com Watchdog
+:srcds
+echo (%time%) srcds started
+start /wait srcds.exe.lnk -console -game "C:\Program Files (x86)\Steam\steamapps\sourcemods\MalMetro" +map hangout +maxplayers 16 -autoupdate ?listen
+echo CRASHED
+pause
+goto srcds
