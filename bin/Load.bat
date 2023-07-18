8ea1d8b2091dc501533ef6ca4ad8c1225b0afec2
Codecrafter <41242242+seagull101@users.noreply.github.com>
2023-07-17 18:40:27 -0700

    Added stuff
    I forgot lol

--- bin/Load.bat
+++ bin/Load.bat
@@ -1,2 +1,2 @@
-Xcopy /C /Y "D:\SteamLibrary\steamapps\sourcemods\MetroSDK\game\server\Release_mod_hl2mp\server.dll" "S:\steamlibrary\sourcemods\Source-Hangout\bin"
-Xcopy /C /Y "D:\SteamLibrary\steamapps\sourcemods\MetroSDK\game\client\Release_mod_hl2mp\client.dll" "S:\steamlibrary\sourcemods\Source-Hangout\bin"
+Xcopy /C /Y "D:\SteamLibrary\steamapps\sourcemods\MetroSDK\game\server\Release_mod_hl2mp\server.dll" "C:\Program Files (x86)\Steam\steamapps\sourcemods\Source-Hangout-GIT\bin"
+Xcopy /C /Y "D:\SteamLibrary\steamapps\sourcemods\MetroSDK\game\client\Release_mod_hl2mp\client.dll" "C:\Program Files (x86)\Steam\steamapps\sourcemods\Source-Hangout-GIT\bin"
